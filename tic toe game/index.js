d8b66a49887622fb342a0c30d597b24f3604e661
pujakumari08102002 <143525098+pujakumari08102002@users.noreply.github.com>
2023-09-24 12:45:48 +0530

    tictactoe
    tic tac toe game  with sound effect

---
+++ tic toe game/index.js
@@ -0,0 +1,89 @@
+console.log("welcome");
+let music =  new Audio('WhatsApp Audio 2023-06-29 at 7.08.29 PM.aac')
+let audioTurn = new Audio("WhatsApp Audio 2023-06-29 at 7.08.28 PM.aac")
+let gameover = new Audio("WhatsApp Audio 2023-06-29 at 7.08.39 PM (1).aac")
+
+let isgameover =  false;
+
+
+
+let turn = "X";
+
+
+//function to change the turn 
+const changeTurn =()=>{
+
+    return turn === "X"? "0": "X"
+}
+
+
+//function to check win
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext')
+    let wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
+    wins.forEach(e=>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !=='') )
+        {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText +"won"
+            isgameover =true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width ='200px' 
+            gameover.play();
+        }
+    })
+
+
+
+}
+
+
+
+
+//game logic
+ music.play();
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click',()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = turn;
+            turn= changeTurn();
+            audioTurn.play();
+            checkWin();
+            if(!isgameover){
+            document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
+     }
+    }
+        // console.log("done");
+
+    })
+
+})
+
+//add onclick listener to reset button
+reset.addEventListener('click',()=>{
+   let boxtexts = document.querySelectorAll('.boxtext');
+   Array.from(boxtexts).forEach(element =>{
+    element.innerText=""
+    turn = "X";
+    isgameover = false;
+
+
+
+    document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width ='0px' 
+
+
+
+   })
+})
+
+
